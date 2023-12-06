082cd8db436b06aa25a58043f69eb1cc75dc7c9f
Nelson Ortega <ne.beltran18@gmail.com>
2023-12-05 21:17:56 -0500

    feat: created task controller handling basic CRUD operations

---
+++ src/main/java/Controllers/TaskController.java
@@ -0,0 +1,42 @@
+package Controllers;
+
+import DTO.TaskDto;
+import Models.TaskModel;
+import Services.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private TaskService service;
+
+    @GetMapping
+    public @ResponseBody List<TaskModel> getAllTask(){
+        return service.getAllTasks();
+    }
+
+    @GetMapping("/{taskId}")
+    public @ResponseBody TaskModel getTaskById(@PathVariable Long taskId){
+        return service.getById(taskId);
+    }
+
+    @PostMapping
+    public @ResponseBody TaskModel createTask(@RequestBody @Valid TaskDto newTask){
+        return service.createTask(newTask);
+    }
+
+    @PutMapping("/{taskId}")
+    public @ResponseBody TaskModel updateTask(@PathVariable Long taskId, @RequestBody TaskDto updatedTask){
+        return service.updateTask(taskId, updatedTask);
+    }
+
+    @DeleteMapping("/{taskId}")
+    public void deleteTask(@PathVariable Long taskId){
+        service.deleteTask(taskId);
+    }
+
+}
